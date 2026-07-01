247fea6de191aa66594a595fe5d8f36ec81f6387
Suence <suence.online@outlook.com>
2026-07-01 12:43:31 +0800

    :bug: 首页 - 将扩展商店的显示逻辑改为数据驱动

---
+++ SRC/Host/AuraEcho/Models/MarketplaceNavigationItem.cs
@@ -0,0 +1,8 @@
+namespace AuraEcho.Models;
+
+/// <summary>
+/// 首页扩展商店导航项(用于匹配模板选择器)
+/// </summary>
+public class MarketplaceNavigationItem
+{
+}
--- SRC/Host/AuraEcho/Selectors/AppPluginDataTemplateSelector.cs
+++ SRC/Host/AuraEcho/Selectors/AppPluginDataTemplateSelector.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using AuraEcho.Api.Models.V1.Plugin;
 using AuraEcho.Core.Models;
+using AuraEcho.Models;
 
 namespace AuraEcho.Selectors;
 
@@ -11,9 +12,13 @@ public class AppPluginDataTemplateSelector : DataTemplateSelector
     public DataTemplate? StandalonePluginTemplate { get; set; }
     public DataTemplate? LocalWebPluginTemplate { get; set; }
     public DataTemplate? RemoteWebPluginTemplate { get; set; }
+    public DataTemplate? MarketplaceNavigationTemplate { get; set; }
 
     public override DataTemplate? SelectTemplate(object item, DependencyObject container)
     {
+        if (item is MarketplaceNavigationItem)
+            return MarketplaceNavigationTemplate;
+
         if (item is not AppPlugin plugin) return null;
 
         return plugin.PluginType switch
